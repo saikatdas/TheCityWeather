ff0cc09b08b45f7a99c34cedab3c796bb56722e2
saikatdas <dassaikat93@yahoo.com>
2019-04-06 11:45:05 +0530

    with minimum data

---
+++ VueApp.js
@@ -0,0 +1,14 @@
+const vm = new Vue({
+    el: '#app',
+    data : {
+           data:null,
+        },
+       mounted () {
+         axios.get('http://api.openweathermap.org/data/2.5/weather?id=1264527&appid=e5116af8e6401f9e6180bd21ebc00124&units=metric')
+      .then(response => {
+        console.log(response)
+        this.data = response.data
+      })
+ 
+      }
+ });
