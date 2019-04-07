fa180e4cd8dde6667a76ad379ef08465ceebbcea
saikatdas <dassaikat93@yahoo.com>
2019-04-07 12:35:09 +0530

    added new code

--- VueApp.js
+++ VueApp.js
@@ -5,10 +5,13 @@ const vm = new Vue({
         },
        mounted () {
          axios.get('http://api.openweathermap.org/data/2.5/weather?id=1264527&appid=e5116af8e6401f9e6180bd21ebc00124&units=metric')
+        // 5days forecast
+        // https://api.openweathermap.org/data/2.5/forecast?id=1264527&appid=e5116af8e6401f9e6180bd21ebc00124&units=metric
       .then(response => {
         console.log(response)
         this.data = response.data
       })
  
       }
- });
+ }
+ );
